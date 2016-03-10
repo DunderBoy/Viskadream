baf7bc11e013fbdf3a940bc46c7197cbe5981c81
Johan Hedén <heden.johan@gmail.com>
2016-03-10 14:33:46 +0100

    Added viskadream project scourcecode

---
+++ Viskadream/Viskadream.Droid/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using SupportToolbar = Android.Support.V7.Widget.Toolbar;
+using Android.Support.V7.App;
+
+namespace Viskadream.Droid
+{
+	[Activity (Label = "Viskadream.Droid", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/viskadreamTheme")]
+	public class MainActivity : ActionBarActivity
+	{
+        private SupportToolbar mToolbar;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+            
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+            mToolbar = FindViewById<SupportToolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(mToolbar);
+		}
+	}
+}
+
+
